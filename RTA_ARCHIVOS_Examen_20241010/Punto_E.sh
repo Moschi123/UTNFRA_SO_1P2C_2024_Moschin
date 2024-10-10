ba48f46f6a7f7e802c28ea8a40e922a569cb7df8
Moschi123 <francoeduardo500@gmail.com>
2024-10-10 02:20:42 +0000

    delete:archivos mal direccionados,eliminados

--- RTA_ARCHIVOS_Examen_20241010/Punto_E.sh
+++
@@ -1,8 +0,0 @@
-cat << EOF > ~/repogit/UTNFRA_SO_1P2C_2024_Moschin/RTA_ARCHIVOS_Examen_20241010/Filtro_Basico.txt
-
-Total de Memoria RAM: $(grep -i "memtotal" /proc/meminfo)
-
-Fabricante del Chasis: $(sudo dmidecode -t chassis | grep -i "manufacturer")
-
-EOF
-
