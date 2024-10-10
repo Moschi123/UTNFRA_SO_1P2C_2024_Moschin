9c7208709268cbd4d2f6064c306f6bd7b1f34953
Moschi123 <francoeduardo500@gmail.com>
2024-10-10 02:06:19 +0000

    feat:nuevas carpetas

---
+++ RTA_SCRIPT_Examen_20241010/Punto_D.sh
@@ -0,0 +1,4 @@
+mkdir -p ~/Estructura_Asimetrica/correo/cartas_{1..100}
+mkdir -p ~/Estructura_Asimetrica/clientes/cartas_{1..100}
+mkdir -p ~/Estructura_Asimetrica/correo/cartas_1/carteros_{1..10}
+
